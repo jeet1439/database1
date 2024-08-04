b55631b07cb1282394c5ba930463935f09de570f
jeet <jeetbanik332004@gmail.com>
2024-08-05 03:03:17 +0530

    complete-backend

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# database1
-This app clarifies all concept of basic CRUD operation with database and RESTful api's.
+# Overview
+This project is a database management system built using Node.js and several key dependencies. It is designed to provide a robust and efficient solution for managing and interacting with a database, featuring tools for generating fake data, rendering templates, handling HTTP requests, and managing unique identifiers.
+<br>
+<h3>Package Versions</h3><br>
+@faker-js/faker: ^8.4.1 --generating fake data<br>
+ejs: ^3.1.10 --creating the UI<br>
+express: ^4.19.2 -- for writting backend<br>
+method-override: ^3.0.0 --for implementing PATCH & DELETE request<br>
+mysql2: ^3.11.0 --conneting with mysql database<br>
+uuid: ^10.0.0 --generating unique id<br>
---
+++ index.js
@@ -0,0 +1,173 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+//for id generation:
+const { v4: uuidv4 } = require('uuid');
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "/views"));
+app.use(methodOverride("_method"));
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jeets_app',
+    password: 'root@304*A90'
+});
+//for pushing the data:
+// let q = "INSERT INTO user (id, username, email, password) VALUES(?, ?, ?, ?)";
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let users = [
+//     ["123b", "123_newuserb", "abc@gmail.comb", "passbcb"],
+//     ["123c", "123_newuserc", "abc@gmail.comc", "passbcc"]
+// ];
+let getRandomUser = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.userName(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+};
+// let data = [];
+// for(let i=0; i<100; i++){
+//    data.push(getRandomUser());
+// }
+
+// connection.query(q , [data], (err, result) => {
+//     if (err) {
+//         console.error('Error executing query:', err);
+//         return;
+//     }
+//     console.log('Tables in database:', result);
+//     // console.log(result[1]);
+
+// });
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//         return;
+//     }
+//     console.log('Connected to the database');
+// });
+// connection.end();
+// let getRandomUser = () => {
+//     return {
+//         id: faker.string.uuid(),
+//         username: faker.internet.userName(),
+//         email: faker.internet.email(),
+//         password: faker.internet.password(),
+//     };
+// };
+
+// Ensure to close the connection after you're done
+// connection.end();
+
+app.get("/", (req, res) => {
+    let q = `SELECT count(*) FROM user`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let count = result[0]["count(*)"];
+            // res.send("sucess"); 
+            res.render("home.ejs", { count });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("some error in db");
+    }
+});
+
+//show route:
+app.get("/user", (req, res) => {
+    let q = `SELECT * FROM user`;
+    try {
+        connection.query(q, (err, users) => {
+            if (err) throw err;
+            // res.send("sucess"); 
+            res.render("showUser.ejs", { users });
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("some error in db");
+    }
+});
+//edit -route: only getting the form for edit
+app.get("/user/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", { user });
+            //        console.log(user);
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("some error in db");
+    }
+});
+//update route:
+app.patch("/user/:id", (req, res) => {
+    let { password: formPassword, username: newUsername } = req.body;
+    let { id } = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (formPassword != user.password) {
+                res.send("wrong password");
+            } else {
+                let q2 = `UPDATE user SET username= '${newUsername}' WHERE id='${id}'`;
+                connection.query(q2, (err, result) => {
+                    if (err) throw err;
+                    res.redirect("/user");
+                })
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("some error in db");
+    }
+});
+//adding new user:
+app.get("/user/new", (req, res) => {
+    let id = uuidv4();
+    res.render("addUser.ejs", { id });
+});
+
+app.get("/render", (req, res) => {
+    res.render("rendermain.ejs");
+})
+app.post("/user/new", (req, res) => {
+    let { username, email, password, id } = req.body;
+    let checkEmailQuery = 'SELECT COUNT(*) AS count FROM user WHERE email = ?';
+    connection.query(checkEmailQuery, [email], (err, results) => {
+        if (err) throw err;
+        if (results[0].count > 0) {
+            // res.send("Email already exists");
+            res.redirect("/render");
+        } else {
+            let q = `INSERT INTO user (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+            try {
+                connection.query(q, (err, result) => {
+                    if (err) throw err;
+                    console.log("added new user");
+                    res.redirect("/user");
+                });
+            } catch (err) {
+                res.send("some error occurred");
+            }
+        }
+    });
+});
+app.listen("3000", () => {
+    console.log("server listening at port 3000");
+});
